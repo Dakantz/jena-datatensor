ba977c2cfb29f947eaf1708226ef92c46c3ec571
Benedikt Kantz <benedikt@kantz.at>
2026-08-19 11:22:51 +0200

    Timing util

--- jena-datatensor/src/main/java/pl/edu/pw/mini/jena/datatensor/TimingUtil.java
+++ jena-datatensor/src/main/java/pl/edu/pw/mini/jena/datatensor/TimingUtil.java
@@ -1,35 +1,37 @@
 package pl.edu.pw.mini.jena.datatensor;
 
+import java.time.Instant;
+import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-
 public class TimingUtil {
 
-    private static final Logger log = LoggerFactory.getLogger(TimingUtil.class);
-    private static TimingUtil instance;
+  private static final Logger log = LoggerFactory.getLogger(TimingUtil.class);
+  private static TimingUtil instance;
 
-    public static TimingUtil timer() {
-        if (instance == null) {
-            instance = new TimingUtil();
-        }
-        return instance;
+  public static TimingUtil timer() {
+    if (instance == null) {
+      instance = new TimingUtil();
     }
+    return instance;
+  }
 
-    private TimingUtil() {
-    }
+  private TimingUtil() {}
 
-    private HashMap<String, Long> startTimes = new HashMap<>();
+  private HashMap<String, Long> startTimes = new HashMap<>();
+  private static long currentNanos() {
+    return System.nanoTime();
+  }
+  public void startTimer(String label) {
+    log.info(String.join("", "--TIMING (", label, ") START--"));
+    startTimes.put(label, currentNanos());
+  }
 
-    public void startTimer(String label) {
-        log.info(String.join("--TIMING(", label, "( START--"));
-        startTimes.put(label, System.currentTimeMillis());
-    }
-
-    public void stopTimer(String label) {
-        var endTime = System.currentTimeMillis();
-        var difference = endTime - startTimes.get(label);
-        log.info(String.join("", "--TIMING(", label, "( END: ", String.valueOf(difference), "  ms--"));
-    }
+  public void stopTimer(String label) {
+    var endTime = currentNanos();
+    var difference = endTime - startTimes.get(label);
+    log.info(String.join("", "--TIMING (", label,
+                         ") END: ", String.valueOf(difference), "  ns--"));
+  }
 }
